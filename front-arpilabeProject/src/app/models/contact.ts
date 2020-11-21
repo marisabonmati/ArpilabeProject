bb739ee7b8b2cd94eaf00ea2c506a99a3875a4d3
marisabonmati <68008846+marisabonmati@users.noreply.github.com>
2020-11-21 10:33:58 +0100

    CRUD class

---
+++ front-arpilabeProject/src/app/models/contact.ts
@@ -0,0 +1,10 @@
+export class Comentario {
+    id?: number;
+    prenom: string;
+    nom: string;
+    email: string;
+    telephone: number;
+    note: string;
+    departement: string;
+    dateDeNaissance: Date;
+}

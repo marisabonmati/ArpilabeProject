9f8dad9090a592ce782ca943a5cbf313cdf96d93
marisabonmati <68008846+marisabonmati@users.noreply.github.com>
2020-11-21 10:52:49 +0100

    Routes components

--- front-arpilabeProject/src/app/app-routing.module.ts
+++ front-arpilabeProject/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AjouterEditerContactComponent } from './components/ajouter-editer-contact/ajouter-editer-contact.component';
+import { ListContactsComponent } from './components/list-contacts/list-contacts.component';
+import { VoirDetailsComponent } from './components/voir-details/voir-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'ajouter', component: AjouterEditerContactComponent},
+  {path: 'editer/:id', component: AjouterEditerContactComponent},
+  {path: 'voir/:id', component: VoirDetailsComponent},
+  {path: '', component: ListContactsComponent, pathMatch: 'full'},
+  {path: '**', redirectTo: '/'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

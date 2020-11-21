35d21d1b5e35fadca3224dbaed85a144c9824df0
marisabonmati <68008846+marisabonmati@users.noreply.github.com>
2020-11-21 11:24:46 +0100

    Validators - Warnings

--- front-arpilabeProject/src/app/app.module.ts
+++ front-arpilabeProject/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { VoirDetailsComponent } from './components/voir-details/voir-details.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-arpilabeProject/src/app/components/ajouter-editer-contact/ajouter-editer-contact.component.ts
+++ front-arpilabeProject/src/app/components/ajouter-editer-contact/ajouter-editer-contact.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ajouter-editer-contact',
   templateUrl: './ajouter-editer-contact.component.html',
   styleUrls: ['./ajouter-editer-contact.component.css']
 })
+
 export class AjouterEditerContactComponent implements OnInit {
 
-  constructor() { }
+  contact: FormGroup;
+  idContact = 0;
+  action = 'Ajouter un contact';
+
+  constructor(private fb: FormBuilder, private route: ActivatedRoute) {
+    this.contact = this.fb.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      dateDeNaissance: ['', Validators.required],
+      telephone: ['', Validators.required],
+      email: ['', Validators.required],
+      note: ['', Validators.required],
+      departement: ['', Validators.required],
+    });
+
+    if (+this.route.snapshot.paramMap.get('id')! > 0) {
+      this.idContact = +this.route.snapshot.paramMap.get('id')!;
+    }
+
+  }
+
 
   ngOnInit(): void {
+    this.esEditer();
+  }
+
+  sauvgarderContact(){
+    console.log(this.contact);
+  }
+
+  esEditer() {
+    if (this.idContact > 0) {
+      this.action = 'Éditer le contact';
+      this.contact.patchValue({
+        nom: 'Bonmati',
+        prenom: 'Marisa',
+        dateDeNaissance: '17/04/90',
+        telephone: '333333333',
+        email: 'hola@hola.com',
+        departement: 'IT',
+        note: 'ok'
+      })
+    }
   }
 
 }

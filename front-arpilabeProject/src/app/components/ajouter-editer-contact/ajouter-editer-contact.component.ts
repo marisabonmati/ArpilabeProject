81111dbec286436161be5521daf908686e30f08c
marisabonmati <68008846+marisabonmati@users.noreply.github.com>
2020-11-22 15:57:18 +0100

    new contact

--- front-arpilabeProject/src/app/components/ajouter-editer-contact/ajouter-editer-contact.component.ts
+++ front-arpilabeProject/src/app/components/ajouter-editer-contact/ajouter-editer-contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Contact } from 'src/app/models/contact';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-ajouter-editer-contact',
@@ -14,7 +16,7 @@ export class AjouterEditerContactComponent implements OnInit {
   idContact = 0;
   action = 'Ajouter un contact';
 
-  constructor(private fb: FormBuilder, private route: ActivatedRoute) {
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private contactService: ContactService, private router: Router) {
     this.contact = this.fb.group({
       nom: ['', Validators.required],
       prenom: ['', Validators.required],
@@ -37,6 +39,21 @@ export class AjouterEditerContactComponent implements OnInit {
   }
 
   sauvgarderContact(){
+    if(this.action === "Ajouter"){
+      const contact: Contact = {
+        dateDeNaissance: new Date(),
+        nom: this.contact.get('nom').value,
+        prenom: this.contact.get('prenom').value,
+        departement: this.contact.get('departement').value,
+        note: this.contact.get('note').value,
+        email: this.contact.get('email').value,
+        telephone: this.contact.get('telephone').value,
+      };
+
+      this.contactService.saveContact(contact).subscribe(data =>{
+        this.router.navigate(['/']);
+      })
+    }
     console.log(this.contact);
   }
 
